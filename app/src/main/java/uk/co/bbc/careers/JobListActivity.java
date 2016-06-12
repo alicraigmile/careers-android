76d4da1184399be93e15df66e0fc9aa1662d8152
Ali Craigmile <ali.craigmile@bbc.co.uk>
2016-06-12 23:19:16 +0100

    feature(jobs list) remove redundant email button from jobs list

--- app/src/main/java/uk/co/bbc/careers/JobListActivity.java
+++ app/src/main/java/uk/co/bbc/careers/JobListActivity.java
@@ -59,18 +59,6 @@ public class JobListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-
-        // Refresh when you click the floating action button
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                doAsyncRefresh();
-                Snackbar.make(view, "Refreshing jobs...", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         View recyclerView = findViewById(R.id.job_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);

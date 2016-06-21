9bac04f5efb8ca245a97dd6adc1b66eb6d311e28
Ali Craigmile <ali.craigmile@bbc.co.uk>
2016-06-21 12:06:47 +0100

    feature(jobs list) sort locations so largest centres are first

--- app/src/main/java/uk/co/bbc/careers/Jobs.java
+++ app/src/main/java/uk/co/bbc/careers/Jobs.java
@@ -48,13 +48,29 @@ class GradeComparator implements Comparator<Job> {
 }
 
 class LocationComparator implements Comparator<Job> {
+
+    //TODO - this could be loaded from a config file on init
+    static ArrayList<String> jobLocations = new ArrayList<>();
+    static {
+        jobLocations.add("London");
+        jobLocations.add("MediaCity UK, Salford Quays");
+        jobLocations.add("Birmingham");
+        jobLocations.add("English Regions");
+        jobLocations.add("Scotland");
+        jobLocations.add("Wales");
+        jobLocations.add("Northern Ireland");
+        jobLocations.add("International");
+        jobLocations.add("Other");
+    };
+
     @Override
     public int compare(Job a, Job b) {
         try {
             String a2 = LocationSectionCalculator.groupingOf(a.location);
             String b2 = LocationSectionCalculator.groupingOf(b.location);
 
-            return a2.compareToIgnoreCase(b2);
+            return Integer.compare(jobLocations.indexOf(a2), jobLocations.indexOf(b2));
+
         } catch (NullPointerException e) {
             return 0;
         }
